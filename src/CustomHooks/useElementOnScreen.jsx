a7231ca115b04e88d786caa36c24c8e4c8bb30d6
Belgarde Jean Ronel <belganel@gmail.com>
2023-07-20 09:31:00 +0200

    update app with api Intersection Observer

--- src/CustomHooks/useElementOnScreen.jsx
+++ src/CustomHooks/useElementOnScreen.jsx
@@ -1,28 +1,26 @@
 import { useEffect, useRef, useState } from "react";
 
+export function useElementOnScreen(options) {
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
 
-export function useElementOnScreen (options) {
+  const handleIntersect = function (entries) {
+    const [entry] = entries;
 
-    const ref = useRef(null);
-    const [ isVisible, setIsVisible ] = useState(false);
-  
-    const handleIntersect = function (entries, observer) {
-
-      const [entry] = entries;
-      
-        setIsVisible(entry.isIntersecting);
-        // observer.unobserve(entry.target);
-
+    if (entry.intersectionRatio > options.threshold) {
+      setIsVisible(entry.isIntersecting);
     }
-  
-    useEffect(() => {
-      const observer = new IntersectionObserver(handleIntersect, options);
-      if (ref.current) observer.observe(ref.current);
-  
-      return () => {
-        if (ref.current) observer.unobserve(ref.current);
-      }
-    }, [ref, options]);
-  
-    return [ref, isVisible];
+
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersect, options);
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  }, []);
+
+  return [ref, isVisible];
 }
